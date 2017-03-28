71855929373b0f07875c6d8f5f00432a1a22144a
albusma@gmail.com <albusma@gmail.com>
2017-03-27 22:35:50 -0700

    adjust code to improve accuracy

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -66,9 +66,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
     // first measurement
-    cout << "EKF: " << endl;
+    //cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    ekf_.x_ << 0, 0, 0, 0;
 
     // Initial transition matrix, F
     ekf_.F_ = MatrixXd(4, 4);
@@ -105,14 +105,17 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       */
       float rho = measurement_pack.raw_measurements_[0];
       float phi = measurement_pack.raw_measurements_[1];
-
+      float rho_dot = measurement_pack.raw_measurements_[2];
       float px = rho * cos(phi);
       float py = rho * sin(phi);
+      float vx = rho_dot * cos(phi);
+      float vy = rho_dot * sin(phi);
+
       if (px == 0 or py == 0){
         return;
       }
 
-      ekf_.x_ << px, py, 0, 0;
+      ekf_.x_ << px, py, vx, vy;
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
@@ -155,8 +158,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   	ekf_.F_(1, 3) = dt;
 
   	//set the process covariance matrix Q
-    float noise_ax = 9;
-    float noise_ay = 9;
+    float noise_ax = 6.25;
+    float noise_ay = 6.25;
 
   	ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
   			   0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
@@ -206,6 +209,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   }
 
   // print the output
-  cout << "x_ = " << ekf_.x_ << endl;
-  cout << "P_ = " << ekf_.P_ << endl;
+  //cout << "x_ = " << ekf_.x_ << endl;
+  //cout << "P_ = " << ekf_.P_ << endl;
 }
